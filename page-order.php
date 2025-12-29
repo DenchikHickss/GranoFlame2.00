07658ce1126133a42abf0ef1706a3d8add7fdf5d
DenchikHickss <denbla01@easv365.dk>
2025-12-29 15:52:46 +0100

    first commit

---
+++ page-order.php
@@ -0,0 +1,13 @@
+<?php
+
+
+get_header();
+
+
+$place_name     = get_field('place_name');
+$place_address  = get_field('place_address');
+
+$here_time      = get_field('here_time');
+$pickup_time    = get_field('pickup_time');
+$delivery_time  = get_field('delivery_time');
+?>
